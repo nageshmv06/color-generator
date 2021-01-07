1ae629989fa9438fe6466da8ff82e9c576ef0ffc
nageshmv06 <nageshmv18@gmail.com>
2021-01-07 20:48:11 +0530

    Updates

---
+++ precache-manifest.82d3b40f0759ebdf1768e51276773b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a410246cbdf63e25b81059b71dba54",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "cb750b75e1a51f3fc01a",
+    "url": "/color-generator/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "e2f04fb3c7f8121c70c2",
+    "url": "/color-generator/static/js/2.adb8e479.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator/static/js/2.adb8e479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb750b75e1a51f3fc01a",
+    "url": "/color-generator/static/js/main.e7dc08c5.chunk.js"
+  },
+  {
+    "revision": "a1cfb88a1dc1d9e37dd7",
+    "url": "/color-generator/static/js/runtime-main.8a33e40c.js"
+  }
+]);
